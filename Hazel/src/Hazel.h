eb5c874fadf17a39c21dc05696446ddbfbe8db23
Miguel Marcano <miguel.marcano@dorst.de>
2023-08-31 21:39:52 +0200

    Set Up Basic Application and Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by Hazel Applications
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// -------- Entry Poiny --------------
+#include "Hazel/EntryPoint.h"
+// -----------------------------------
